4a65fa0afd39f1034b68b611f84cd0adc1416460
Noah Siskind <noahsiskind@mac-pro.mynetworksettings.com>
2025-02-11 08:30:34 -0500

    add pagination

--- app/src/components/emissionsTable.js
+++ app/src/components/emissionsTable.js
@@ -1,10 +1,8 @@
-import { useTable, useSortBy, useFilters} from 'react-table';
+import { useTable, useSortBy, useFilters, usePagination, getPaginationRowModel} from 'react-table';
 import { useState } from 'react';
 import "./emissionsTable.css";
 
 export const EmissionsTable = ({tableData, columns}) => {
-
-    const [data, setData] = useState(tableData);
  
    const {
     getTableProps,
@@ -12,63 +10,109 @@ export const EmissionsTable = ({tableData, columns}) => {
     headerGroups,
     rows,
     prepareRow,
-    state: { sortBy },
+    page,
+    canNextPage,
+    canPreviousPage,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { sortBy, pageIndex, pageSize },
   } = useTable(
     {
       columns,
-      data,
+      data: tableData,
+      initialState: { pageIndex: 0, pageSize: 25 },
     },    
     useFilters,
-    useSortBy
+    useSortBy,
+    usePagination
   );
   
   return (
-    <table {...getTableProps()} style={{ width: '100%', border: '1px solid black' }}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr key={headerGroup.id} {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th key={column.id}>
-                <span key={column.id}
-                  {...column.getHeaderProps(column.getSortByToggleProps())}
-                  style={{
-                    cursor: 'pointer',
-                    backgroundColor: sortBy.some(sort => sort.id === column.id)
-                      ? '#f0f0f0'
-                      : '#fff',
-                  }}
-                >
-                  {column.render('Header')}
-                  <span>
-                    {sortBy.length > 0 && column.id === sortBy[0]?.id ? (
-                      sortBy[0]?.desc ? ' 🔽' : ' 🔼'
-                    ) : ''}
+    <div>
+      <table {...getTableProps()} style={{ width: '100%', border: '1px solid black' }}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr key={headerGroup.id} {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th key={column.id}>
+                  <span key={column.id}
+                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                    style={{
+                      cursor: 'pointer',
+                      backgroundColor: sortBy.some(sort => sort.id === column.id)
+                        ? '#f0f0f0'
+                        : '#fff',
+                    }}
+                  >
+                    {column.render('Header')}
+                    <span>
+                      {sortBy.length > 0 && column.id === sortBy[0]?.id ? (
+                        sortBy[0]?.desc ? ' 🔽' : ' 🔼'
+                      ) : ''}
+                    </span>
                   </span>
-                </span>
-                <input
-                        className={"filterInput"}
-                        type="text"
-                        value={column.filterValue || ""}
-                        onChange={e => column.setFilter(e.target.value)}
-                        placeholder="Filter…"
-                    />
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row);
-          return (
-            <tr td key={row.id} {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return <td key={cell.row.id + cell.column.id} {...cell.getCellProps()}>{cell.render('Cell')}</td>;
-              })}
+                  <input
+                          className={"filterInput"}
+                          type="text"
+                          value={column.filterValue || ""}
+                          onChange={e => column.setFilter(e.target.value)}
+                          placeholder="Filter…"
+                      />
+                </th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map(row => {
+            prepareRow(row);
+            return (
+              <tr td key={row.id} {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td key={cell.row.id + cell.column.id} {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className='options'>
+        <div className='paginationOptions'>
+          <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+            {'<<'}
+          </button>
+          <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+            {'<'}
+          </button>
+          <button onClick={() => nextPage()} disabled={!canNextPage}>
+            {'>'}
+          </button>
+          <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+            {'>>'}
+          </button>
+        </div>
+        <div className='pageSize'>
+          <span>
+            Page{' '}
+            <strong>
+              {pageIndex + 1} of {pageCount}
+            </strong>{' '}
+          </span>
+          <select
+            value={pageSize}
+            onChange={e => setPageSize(Number(e.target.value))}
+          >
+            {[25, 50, 100].map(size => (
+              <option key={size} value={size}>
+                Show {size}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+    </div>
   );
 };
